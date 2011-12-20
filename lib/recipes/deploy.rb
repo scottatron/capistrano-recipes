433127c87d07d14a9d1f5b131aa0d779e9a0379b
Tobias Lindholm <tobias.lindholm@antob.se>
2011-12-20 15:19:12 +0100

    Fixed code update.

--- lib/recipes/deploy.rb
+++ lib/recipes/deploy.rb
@@ -36,7 +36,7 @@ Capistrano::Configuration.instance.load do
 
     desc "|capistrano-recipes| Update the deployed code."
     task :update_code, :roles => :app, :except => { :no_release => true } do
-      run "cd #{current_path}; git checkout #{branch}; git fetch origin; git pull"
+      run "cd #{current_path}; git fetch; git checkout #{branch}; git reset --hard origin/#{branch}"
     end
 
     desc "|capistrano-recipes| Alias for symlinks:make"
