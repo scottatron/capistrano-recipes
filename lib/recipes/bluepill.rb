f8e3dd5daf4579927d4b9eabd71678fd109fe47f
Tobias Lindholm <tobias.lindholm@antob.se>
2012-01-02 21:46:42 +0100

    Modified bluepill recipe.

--- lib/recipes/bluepill.rb
+++ lib/recipes/bluepill.rb
@@ -1,53 +1,44 @@
 Capistrano::Configuration.instance.load do  
   namespace :bluepill do
-    desc "|capistrano-recipes| Install the bluepill monitoring tool"
-    task :install, :roles => [:app] do
-      sudo "gem install bluepill"
-    end
-    
     desc "|capistrano-recipes| Stop processes that bluepill is monitoring and quit bluepill"
     task :quit, :roles => [:app] do
       args = exists?(:options) ? options : ''
       begin
-        sudo "bluepill stop #{args}"
+        run "bundle exec bluepill stop #{args}"
       rescue
         puts "Bluepill was unable to finish gracefully all the process"
       ensure
-        sudo "bluepill quit"
+        run "bundle exec bluepill quit"
       end
     end
     
-    desc "|capistrano-recipes| Load the pill from {your-app}/config/pills/{app-name}.pill"
+    desc "|capistrano-recipes| Load the pill from {your-app}/config/unicorn.pill"
     task :init, :roles =>[:app] do
-      sudo "bluepill load #{current_path}/config/pills/#{application}.pill"
+      run "bundle exec bluepill load #{current_path}/config/unicorn.pill"
     end
  
     desc "|capistrano-recipes| Starts your previous stopped pill"
     task :start, :roles =>[:app] do
       args = exists?(:options) ? options : ''
-      sudo "bluepill start #{args}"
+      run "bundle exec bluepill start #{args}"
     end
     
     desc "|capistrano-recipes| Stops some bluepill monitored process"
     task :stop, :roles =>[:app] do
       args = exists?(:options) ? options : ''
-      sudo "bluepill stop #{args}"
+      run "bundle exec bluepill stop #{args}"
     end
     
-    desc "|capistrano-recipes| Restarts the pill from {your-app}/config/pills/{app-name}.pill"
+    desc "|capistrano-recipes| Restarts the pill from {your-app}/config/unicorn.pill"
     task :restart, :roles =>[:app] do
       args = exists?(:options) ? options : ''
-      sudo "bluepill restart #{args}"
+      run "bundle exec bluepill restart #{args}"
     end
  
     desc "|capistrano-recipes| Prints bluepills monitored processes statuses"
     task :status, :roles => [:app] do
       args = exists?(:options) ? options : ''
-      sudo "bluepill status #{args}"
+      run "bundle exec bluepill status #{args}"
     end
   end
-    
-  after 'deploy:setup' do
-    bluepill.install if Capistrano::CLI.ui.agree("Do you want to install the bluepill monitor? [Yn]")
-  end if is_using('bluepill', :monitorer)
 end
