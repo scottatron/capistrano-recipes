b13aaf658fffc9ca31d621f5c155e248512b0c40
Leif Ringstad <leifcr@gmail.com>
2013-01-10 17:03:51 +0100

    Added delayed job to bluepill recipe

--- lib/recipes/bluepill.rb
+++ lib/recipes/bluepill.rb
@@ -1,4 +1,7 @@
 Capistrano::Configuration.instance.load do  
+  set(:bluepill_use_puma)        {true}   unless exists?(:bluepill_use_puma)
+  set(:bluepill_use_delayed_job) {false}  unless exists?(:bluepill_use_delayed_job)
+  set(:bluepill_local_init) { File.join(templates_path, "bluepill_init.conf.erb") } 
   set(:bluepill_local_init) { File.join(templates_path, "bluepill_init.conf.erb") } 
   set(:bluepill_remote_init) { File.join("/etc", "init", "bluepill_#{application}_#{rails_env}.conf") } 
   set(:bluepill_local_config) { File.join(templates_path, "bluepill.pill.erb") } 
@@ -6,10 +9,22 @@ Capistrano::Configuration.instance.load do
   set(:bluepill_puma_pid) { File.join(pids_path,"#{app_server}.pid") } unless exists?(:bluepill_puma_pid)
   set(:bluepill_working_dir) {"#{current_path}"} unless exists?(:bluepill_working_dir)
   set(:bluepill_app) {"#{application}_#{rails_env}"} unless exists?(:bluepill_app)
-  set(:bluepill_start_grace_time) { "25.seconds" } unless exists?(:bluepill_start_grace_time)
-  set(:bluepill_stop_grace_time)  { "15.seconds" } unless exists?(:bluepill_stop_grace_time)
+  set(:bluepill_start_grace_time)   { "20.seconds" } unless exists?(:bluepill_start_grace_time)
+  set(:bluepill_stop_grace_time)    { "15.seconds" } unless exists?(:bluepill_stop_grace_time)
+  set(:bluepill_restart_grace_time) { "25.seconds" } unless exists?(:bluepill_restart_grace_time)
+  set(:bluepill_delayed_job_pid) { File.join(pids_path,"delayed_job.pid") } unless exists?(:bluepill_delayed_job_pid)
 
+  # def bluepill_remote_dir_exists?(full_path)
+  #   'true' ==  capture("if [ -d /var/run/bluepill ]; then echo 'true'; fi").strip
+  # end
   namespace :bluepill do
+    desc "|capistrano-recipes| Create /var/run/bluepill with correct owner"
+    task :create_var_run, :roles => [:app] do
+      if 'true' !=  capture("if [[ -d /var/run/bluepill ]]; then echo 'true'; fi").strip
+        sudo "mkdir -p /var/run/bluepill"
+        sudo "sudo chown #{user}:#{group} /var/run/bluepill"
+      end
+    end
     desc "|capistrano-recipes| Stop processes that bluepill is monitoring and quit bluepill"
     task :quit, :roles => [:app] do
       args = exists?(:options) ? options : ''
@@ -75,8 +90,9 @@ Capistrano::Configuration.instance.load do
   end
 
   after 'deploy:setup' do
-    bluepill.setup_config if Capistrano::CLI.ui.agree("Create master.pill configuration file? [Yn]")
-    bluepill.setup_init   if Capistrano::CLI.ui.agree("Create #{bluepill_remote_init} configuration file? [Yn]")
+    bluepill.setup_config   if Capistrano::CLI.ui.agree("Create master.pill configuration file? [Yn]")
+    bluepill.setup_init     if Capistrano::CLI.ui.agree("Create #{bluepill_remote_init} configuration file? [Yn]")
+    bluepill.create_var_run if Capistrano::CLI.ui.agree("Create /var/run/bluepill directory? [Yn]")
   end if is_using('bluepill', :monitorer)
 
   # after "deploy:update" do 
