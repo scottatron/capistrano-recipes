a7f8072d1955e64583093270ce119d119117a43c
Scott Arthur <scott@scottatron.com>
2013-06-22 17:31:41 +1000

    Only load basic recipes by default

--- lib/capistrano_recipes.rb
+++ lib/capistrano_recipes.rb
@@ -1,5 +1,8 @@
 require 'capistrano'
 require 'capistrano/cli'
 require 'helpers'
+$LOAD_PATH << File.join(File.dirname(__FILE__), 'lib')
+require 'recipes/deploy'
+require 'recipes/hooks'
+require 'recipes/symlinks'
 
-Dir.glob(File.join(File.dirname(__FILE__), '/recipes/*.rb')).sort.each { |f| load f }
