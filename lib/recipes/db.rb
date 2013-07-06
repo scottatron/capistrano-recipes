271a7baebb9a80225ca6d173936044573aa6a6c6
Scott Arthur <scott@scottatron.com>
2013-07-06 18:33:11 +1000

    db namespace improvements

--- lib/recipes/db.rb
+++ lib/recipes/db.rb
@@ -3,8 +3,16 @@ require 'erb'
 Capistrano::Configuration.instance.load do
   namespace :db do
     set_default :db_admin_user, 'root'
+    set_default :db_name, 'DATABASE'
+    set_default :db_user, 'USER'
+    set_default :db_pass, 'PASS'
     
     namespace :mysql do
+      
+      set_default (:mysql_grant_sql) do
+        "CREATE DATABASE IF NOT EXISTS #{db_name}; GRANT ALL PRIVILEGES ON #{db_name}.* TO #{db_user}@localhost IDENTIFIED BY '#{db_pass}';"
+      end
+
       desc <<-EOF
       |capistrano-recipes| Performs a compressed database dump. \
       WARNING: This locks your tables for the duration of the mysqldump.
@@ -35,14 +43,24 @@ Capistrano::Configuration.instance.load do
     
       desc "|capistrano-recipes| Create MySQL database and user for this environment using prompted values"
       task :setup, :roles => :db, :only => { :primary => true } do
+        cmd = "mysql -u#{db_admin_user} -p -e\"#{mysql_grant_sql}\""
+        if dry_run
+          logger.debug cmd
+        else
+          prepare_from_yaml
+          run cmd do |channel, stream, data|
+            if data =~ /^Enter password:/
+              pass = Capistrano::CLI.password_prompt "Enter database password for '#{db_admin_user}': "
+              channel.send_data "#{pass}\n" 
+            end
+          end
+        end
+      end
+      
+      task :destroy, :roles => :db, :only => { :primary => true } do
         prepare_from_yaml
-
-        sql = <<-SQL
-        CREATE DATABASE #{db_name};
-        GRANT ALL PRIVILEGES ON #{db_name}.* TO #{db_user}@localhost IDENTIFIED BY '#{db_pass}';
-        SQL
-
-        run "mysql --user=#{db_admin_user} -p --execute=\"#{sql}\"" do |channel, stream, data|
+        sql = "DROP DATABASE IF EXISTS #{db_name};"
+        run "mysql -u#{db_admin_user} -p -e\"#{sql}\"" do |channel, stream, data|
           if data =~ /^Enter password:/
             pass = Capistrano::CLI.password_prompt "Enter database password for '#{db_admin_user}': "
             channel.send_data "#{pass}\n" 
@@ -54,7 +72,7 @@ Capistrano::Configuration.instance.load do
       def prepare_from_yaml
         set(:db_file) { "#{application}-dump.sql.bz2" }
         set(:db_remote_file) { "#{shared_path}/backup/#{db_file}" }
-        set(:db_local_file)  { "tmp/#{db_file}" }
+        set(:db_local_file)  { db_file }
         set(:db_user) { db_config[rails_env]["username"] }
         set(:db_pass) { db_config[rails_env]["password"] }
         set(:db_host) { db_config[rails_env]["host"] }
@@ -88,7 +106,12 @@ Capistrano::Configuration.instance.load do
         <<: *base
       EOF
 
-      put db_config.result, "#{shared_path}/config/database.yml"
+      ensure_dir_exists "#{shared_path}/config"
+      if dry_run
+        logger.debug "put #{shared_path}/config/database.yml"
+      else
+        put db_config.result, "#{shared_path}/config/database.yml"
+      end
     end
   end
     
@@ -104,9 +127,13 @@ Capistrano::Configuration.instance.load do
     run "cd #{current_path}; rake RAILS_ENV=#{variables[:rails_env]} db:seed"
   end
   
-  after "deploy:setup" do
-    db.create_yaml if Capistrano::CLI.ui.agree("Create database.yml in app's shared path? [Yn]")
+  after 'deploy:setup' do
+    db.create_yaml if Capistrano::CLI.ui.agree("\n--- Create database.yml? [Yn] ")
   end if is_using('mysql', :database)
+  
+  after 'db:create_yaml' do
+    db.mysql.setup if Capistrano::CLI.ui.agree("\n--- Create mysql database and grant privileges? [Yn] ")
+  end if is_using 'mysql', :database
 
   before "symlinks:make" do
     run "rm -f #{release_path}/config/database.yml"
