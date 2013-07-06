f478808112ef5d9e72566b9a4ebd11f3ab95a6dc
Scott Arthur <scott@scottatron.com>
2013-07-06 22:11:09 +1000

    Started moving hooks to single location with conditional switches

--- lib/recipes/hooks.rb
+++ lib/recipes/hooks.rb
@@ -1,6 +1,16 @@
 # Common hooks for all scenarios.
 Capistrano::Configuration.instance.load do
+  
+  after 'deploy:setup' do
+    apache2.setup if is_using 'apache2', :web_server
+    if is_using 'mysql', :database
+      db.create_yaml
+      db.mysql.setup
+    end
+    symlinks.make if github_style?
+  end
+  
   after "deploy:finalize_update" do
-    symlinks.make
+    symlinks.make unless github_style?
   end
 end
