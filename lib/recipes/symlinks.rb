e011de2c1162cc03646bf86801e019326982b232
Scott Arthur <scott@scottatron.com>
2013-07-06 22:16:13 +1000

    Improved symlinks namespace

--- lib/recipes/symlinks.rb
+++ lib/recipes/symlinks.rb
@@ -1,28 +1,17 @@
 Capistrano::Configuration.instance.load do
-  # These are set to the same structure in shared <=> current
-  set :normal_symlinks, %w(log config/database.yml) unless exists?(:normal_symlinks)
   
-  # Weird symlinks go somewhere else. Weird.
-  set :weird_symlinks, { 'bundle'  => 'vendor/bundle',
-                         'pids'    => 'tmp/pids',
-                         'sockets' => 'tmp/sockets' } unless exists?(:weird_symlinks)
+  set_default :normal_symlinks, %w(log config/database.yml)
+  set_default :mapped_symlinks, { 'bundle'  => 'vendor/bundle', 'pids' => 'tmp/pids', 'sockets' => 'tmp/sockets' }
 
   namespace :symlinks do
-    desc "|capistrano-recipes| Make all the symlinks in a single run"
+    desc "Make all the symlinks in a single run"
     task :make, :roles => :app, :except => { :no_release => true } do
-      commands = normal_symlinks.map do |path|
-        "rm -rf #{latest_release}/#{path} && \
-         ln -s #{shared_path}/#{path} #{latest_release}/#{path}"
-      end
-
-      commands += weird_symlinks.map do |from, to|
-        "rm -rf #{latest_release}/#{to} && \
-         ln -s #{shared_path}/#{from} #{latest_release}/#{to}"
-      end
-
-      run <<-CMD
-        cd #{latest_release} && #{commands.join(" && ")}
-      CMD
+      commands = ["cd #{latest_release}"]
+      commands.concat normal_symlinks.map {|path| "rm -rf #{latest_release}/#{path}" }
+      commands.concat normal_symlinks.map {|path| "ln -s #{shared_path}/#{path} #{latest_release}/#{path}" }
+      commands.concat mapped_symlinks.map {|from, to| "rm -rf #{latest_release}/#{to}" }
+      commands.concat mapped_symlinks.map {|from, to| "ln -s #{shared_path}/#{from} #{latest_release}/#{to}" }
+      run commands.join(" && ")
     end
   end
 end
